383f1402d9e58b1668f52ff23f9b71a51fd5859f
Baptiste HANRIAT <baptiste@kianava.de>
2022-01-09 19:31:24 +0100

    Fix max-width of TextInput (not needed anymore)

--- src/components/input/TextInput.tsx
+++ src/components/input/TextInput.tsx
@@ -5,7 +5,6 @@ export const TextInput = styled.input`
   font-family: Inter;
   font-size: 16px;
   height: 50px;
-  max-width: 450px;
   border: none;
   box-shadow: rgba(0, 0, 0, 0.2) 0px 18px 50px -10px;
   padding: 0 20px 0 20px;
